6871890964e91431b041e7f08a0baf6770021f47
csutter <csutter@albany.edu>
2025-07-17 18:34:13 +0000

    Small change to add in the model_prob col which is the max prob

--- CNN/scripts/results_predictions_obslevel.py
+++ CNN/scripts/results_predictions_obslevel.py
@@ -127,8 +127,11 @@ def make_preds(run_tracker = config.trackers_list[0], tracker_rundetails = "", w
 
     df_final["tracker"] = tracker_ident
 
-
     if saveflag:
+        # add col that includes model prob (max across the predicted classes)
+        df_final["model_prob"] = df_final[
+            ["prob_dry", "prob_poor_viz", "prob_snow", "prob_snow_severe", "prob_wet"]
+        ].max(axis=1)
         # same naming as with saved models: {tracker_filebase}_{tracker_rundetails}
         predssaveto = f"{config.preds_path}/{tracker_filebase}_{tracker_rundetails}.csv"
         df_final.to_csv(predssaveto) # saving the preds df to csv for one-off runs    
